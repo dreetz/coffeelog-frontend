261412995f0efa20655726012f3b999ad6067a29
Dennis Reetz <dennis.reetz@gmail.com>
2025-09-07 14:32:36 +0200

    fix: checks if backend url is set for build process

--- src/components/CoffeeListActions.ts
+++ src/components/CoffeeListActions.ts
@@ -1,37 +1,49 @@
 import { ICoffee } from "@/interfaces/ICoffeeLog";
 
 export async function getCoffeeList(): Promise<ICoffee[]> {
-  const resultData: Response = await fetch(
-    `${process.env.BACKEND_URL}/coffee/`,
-    {},
-  );
-  return resultData.json();
+  if (process.env.BACKEND_URL) {
+    const resultData: Response = await fetch(
+      `${process.env.BACKEND_URL}/coffee/`,
+      {},
+    );
+    return resultData.json();
+  } else {
+    return [];
+  }
 }
 
 export async function postCoffeeList(newCoffee: ICoffee): Promise<boolean> {
-  const resultData: Response = await fetch(
-    `${process.env.BACKEND_URL}/coffee/`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+  if (process.env.BACKEND_URL) {
+    const resultData: Response = await fetch(
+      `${process.env.BACKEND_URL}/coffee/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newCoffee),
       },
-      body: JSON.stringify(newCoffee),
-    },
-  );
-  return resultData.ok;
+    );
+    return resultData.ok;
+  } else {
+    return false;
+  }
 }
 
 export async function patchCoffeeList(newCoffee: ICoffee): Promise<boolean> {
-  const resultData: Response = await fetch(
-    `${process.env.BACKEND_URL}/coffee/`,
-    {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
+  if (process.env.BACKEND_URL) {
+    const resultData: Response = await fetch(
+      `${process.env.BACKEND_URL}/coffee/`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newCoffee),
       },
-      body: JSON.stringify(newCoffee),
-    },
-  );
-  return resultData.ok;
+    );
+    return resultData.ok;
+  } else {
+    return false;
+  }
 }
