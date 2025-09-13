7c87a92b1639fca055352f488a2db4655508be49
Dennis Reetz <dennis.reetz@gmail.com>
2025-09-13 16:56:33 +0200

    chore: clean up project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
-import NextLink from "next/link";
 import CoffeeListWeb from "@/components/CoffeeListWeb";
 
 export default function Home() {
